0436f84f40d0756ecf055d831d90b665b77da314
Philip Adeoye <semini08@gmail.com>
2019-03-02 14:52:49 -0500

    Updates

---
+++ precache-manifest.4af34bae0b09e4c99e7fc216b521b04b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cae8f754047bf07760c7ce3e71c661c",
+    "url": "/meals-at-home/static/media/desaturated_fruits.4cae8f75.svg"
+  },
+  {
+    "revision": "499bede7a24a34648676",
+    "url": "/meals-at-home/static/css/main.51ecb209.chunk.css"
+  },
+  {
+    "revision": "75335b0a7bd4a9afd6f8",
+    "url": "/meals-at-home/static/js/1.75335b0a.chunk.js"
+  },
+  {
+    "revision": "d0a08484cb98f2e991be",
+    "url": "/meals-at-home/static/js/runtime~main.d0a08484.js"
+  },
+  {
+    "revision": "9dfa959e2d7390927840cb6681124f7c",
+    "url": "/meals-at-home/static/media/processing.9dfa959e.svg"
+  },
+  {
+    "revision": "499bede7a24a34648676",
+    "url": "/meals-at-home/static/js/main.499bede7.chunk.js"
+  },
+  {
+    "revision": "8808692bbc97daaca0c7fa2f91948572",
+    "url": "/meals-at-home/static/media/fruits.8808692b.svg"
+  },
+  {
+    "revision": "0a5a36c9ddfd9386a01ebf5129963be7",
+    "url": "/meals-at-home/static/media/tumbleweed.0a5a36c9.svg"
+  },
+  {
+    "revision": "01095e2dd3297a1aef15c13e966b8f71",
+    "url": "/meals-at-home/static/media/pink-scream.01095e2d.svg"
+  },
+  {
+    "revision": "da52bbdc638571975b52f207dddbe722",
+    "url": "/meals-at-home/static/media/logo_semi_transparent.da52bbdc.svg"
+  },
+  {
+    "revision": "b05c0eb7c6a399e5bbd9482c42a43a93",
+    "url": "/meals-at-home/index.html"
+  }
+];
